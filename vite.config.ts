fe27a4dbd256065306a92ab206f874e846985927
ananthakrishnan <ananthuk0902@gmail.com>
2025-02-09 12:16:35 +0530

    Configure project for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist', 
-  },
-  base: '/Portfolio/',
+  base: './',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
